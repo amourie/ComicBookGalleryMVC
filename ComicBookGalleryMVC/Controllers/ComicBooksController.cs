65baf5fa02595b8efacb87d9aef534788694a8c0
Amourie Fourie <amourie@gmail.com>
2017-08-20 20:57:23 +1000

    Default action method for ComicBooksController created.

--- ComicBookGalleryMVC/Controllers/ComicBooksController.cs
+++ ComicBookGalleryMVC/Controllers/ComicBooksController.cs
@@ -8,5 +8,14 @@ namespace ComicBookGalleryMVC.Controllers
 {
     public class ComicBooksController : Controller //inherits from base Controller class
     {
+        public string Index()
+        {
+            return "this is the comic books default method";
+        }
+        public string Detail()
+        {
+            return "hello from comic books detail method";
+        }
+
     }
 }
